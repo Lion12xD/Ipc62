d22225255947debd8c640fed8eb865d157d72aa7
Lion12xD <103289468+Lion12xD@users.noreply.github.com>
2023-02-21 17:00:05 -0600

    agregamos cambios a pokemon2.py

--- pokemon2.py
+++ pokemon2.py
@@ -1,14 +1,20 @@
+# Script en python que consulta el api de pokemon 
+# para listar los nombres de pokemon pero se le agrego 
+# interacción para que listaras más pokemons segun se vaya requiriendo. 
+# Contribuyo: Leonardo Hasiel Alvarado Zamora 
+# Fecha: 21/02/2023 
 import requests
-#
+# Nombre: Leonardo Hasiel Alvarado Zamora
+# Matricula: 1949295
 def get_pokemons (url='http://pokeapi.co/api/v2/pokemon-form/', offset=0):
-    args = { 'offset':offset } if offset else {}
-    
+    args = {'offset':offset} if offset else {}
+
     response = requests.get(url, params=args)
-    
+
     if response.status_code == 200:
         payload = response.json()
         results = payload.get('results',[])
-        
+
         if results:
             for pokemon in results:
                 name = pokemon['name']
@@ -16,6 +22,6 @@ def get_pokemons (url='http://pokeapi.co/api/v2/pokemon-form/', offset=0):
         next = input ("¿Continuar listando? [Y/N]").lower()
         if next == 'y':
             get_pokemons(offset=offset+20)
-if __name__== '__main__':
-    url='http://pokeapi.co/api/v2/pokemon-form/'
+if __name__ == '__main__':
+    url = 'http://pokeapi.co/api/v2/pokemon-form/'
     get_pokemons()
